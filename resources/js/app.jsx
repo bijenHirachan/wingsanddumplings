0e46c9283da4ac150a693a45ed19756a621add93
Bijen Hirachan <bijen.hirachan@gmail.com>
2025-10-28 21:42:12 +0100

    app.jsx updated

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,8 +3,6 @@ import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-console.log(import.meta.env.VITE_RECAPTCHA_SITE_KEY);
-
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
   setup({ el, App, props }) {
